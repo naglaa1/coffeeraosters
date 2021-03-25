91cc5e333c5c65a75d5b8b85408768fac4eb8d71
naglaa fouz <sama_hap@yahoo.com>
2021-03-25 22:44:50 +0100

    start js for navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
+//for navbar responsive
+const nav = document.querySelector(".header-links nav");
+console.log(nav);
 const hamburge = document.querySelector(".hambIcon");
 const navLinks = document.querySelector(".header-links .navbar");
 const closeLink = document.querySelector(".header-links .close");
 console.log(closeLink);
 
-hamburge.addEventListener('click',() => {
-    hamburge.classList.toggle("hidden");
-    navLinks.classList.add("show");
-    closeLink.classList.toggle("play");
+// on click hamburage 
+hamburge .addEventListener('click' , () => {
+    hamburge.classList.toggle('hidden');
+    closeLink.classList.toggle('show');
+    navLinks.classList.toggle('active');
+});
+// on click close btn
+closeLink.addEventListener('click' , () => {
+    closeLink.classList.toggle('show');
+    hamburge.classList.toggle('hidden');
+    navLinks.classList.toggle('active');
 })
