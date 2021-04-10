5047bc3aa3b159a77c7ff9486ef4576b247c9638
naglaa fouz <sama_hap@yahoo.com>
2021-04-10 23:50:11 +0200

    just start in about page

--- js/main.js
+++ js/main.js
@@ -2,19 +2,25 @@
 const nav = document.querySelector(".header-links nav");
 console.log(nav);
 const hamburge = document.querySelector(".hambIcon");
+console.log(hamburge);
 const navLinks = document.querySelector(".header-links .navbar");
+console.log(navLinks);
 const closeLink = document.querySelector(".header-links .close");
 console.log(closeLink);
 
 // on click hamburage 
-hamburge .addEventListener('click' , () => {
+hamburge.addEventListener('click' , () => {
     hamburge.classList.toggle('hidden');
     closeLink.classList.toggle('show');
     navLinks.classList.toggle('active');
+    nav.classList.toggle('full');
+    // nav.style.backgroundImage = 'linear-gradient(to bottom, rgba, yellow)';
 });
 // on click close btn
 closeLink.addEventListener('click' , () => {
     closeLink.classList.toggle('show');
     hamburge.classList.toggle('hidden');
     navLinks.classList.toggle('active');
-})
+    nav.classList.toggle('full');
+    // nav.style.backgroundImage = 'none'
+});
